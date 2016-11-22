7b4e4a50f00af7d98fbb71917ce77adad785013a
OniReimu <reimu-saber@hotmail.com>
2016-11-22 16:20:07 +1100

    Update installNginx.sh

--- installNginx.sh
+++ installNginx.sh
@@ -7,5 +7,5 @@ cd /vagrant
 ansible-playbook playbook.yml
 mkdir /etc/nginx/html
 git clone https://github.com/OniReimu/Docker_provider_mac.git
-cp /vagrant/Dockertest/default /etc/nginx/sites-available/default
+cp /vagrant/default /etc/nginx/sites-available/default
 /usr/sbin/nginx
